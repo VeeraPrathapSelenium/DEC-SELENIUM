6e564ee6d8c8cad7e02662f6d7642e2562f99d6f
Prathap <prathap.ufttest@gmail.com>
2019-12-26 09:36:41 +0530

    Looping statements

---
+++ src/com/loopingstatements/ForLoop.java
@@ -0,0 +1,25 @@
+package com.loopingstatements;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+
+		//Print the nubers from 1 to 10
+		
+		
+		
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(i);
+			
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
